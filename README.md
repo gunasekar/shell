f3f726ff245737158df9b2719404761ea5ed98d2
Gunasekaran Namachivayam <guna@Gunasekarans-MacBook-Pro.local>
2017-08-26 00:47:43 +0530

    Adding the run-control.sh and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# shell
-Set up your shell with custom environment settings
+#shell
+
+run-control.sh is a script to set up your shell with productive environment settings
+
+##Requirements
+
+Ideally any shell should be supported. bash and zsh shells are tested though. Some commands are supported only if the corresponding packages are available.
+
+##How to use
+Download the script:
+
+```sh
+curl --remote-name https://raw.githubusercontent.com/gunasekar/shell/master/run-control.sh
+```
+
+or
+
+```sh
+wget https://raw.githubusercontent.com/gunasekar/shell/master/run-control.sh
+```
+
+Review the script (avoid running scripts you haven't read!):
+
+```sh
+less run-control.sh
+```
+
+Apply the downloaded run-controls by appending the following line in your .bashrc or .zshrc:
+
+```sh
+source run-control.sh
+```
+
+##What it provides you
+
+Commands and aliases which helps increase your productivity,
+
+1. `uts` prints the unix time stamp
+2. `load-bash` loads the .bashrc
+3. `load-zsh` loads the .zshrc
+4. `notify-after <seconds>` notifies you after given number of seconds and `notify-on-completion <pid>` notifies upon the completion of given PID
+5. `start-mysql` starts the mysql server if not running
+6. `go-build-linux` builds your go project for linux environment (useful when working in mac)
+7. `redis-start` starts the redis server provided redis is installed
+8. `redis-stop` stops the redis server provided redis is installed
+9. `show-aws-cred` prints your AWS access secrets
+10. `docker-stop-all` stops all the running containers
+11. `docker-start-all` starts all the containers
+12. `docker-rm-all-containers` removes all the containers
+13. `docker-rm-all-images` removes all the images
+14. `docker-ips` prints the network details of the docker containers
+15. `dl-audio` downloads the audio in highest available quality in mp3 format using youtube-dl for the provided url
+16. `dl-video` downloads the video in highest available quality using youtube-dl for the provided url
+17. `play` plays the media files from the current directory using mplayer in a shuffled manner
+
+
+##License
+
+shell is free software and may be redistributed under the terms specified in the [LICENSE] file.
+
+[LICENSE]: LICENSE
---
+++ run-control.sh
@@ -0,0 +1,128 @@
+##### general
+alias load-bash="source ~/.bashrc"
+alias load-zsh="source ~/.zshrc"
+alias uts="date +%s"
+alias play="mpv -shuffle *"
+alias gs="git status"
+alias gc="git commit"
+
+##### aws
+export AWS_SDK_LOAD_CONFIG=1
+
+function show-aws-creds {
+	cat ~/.aws/credentials
+}
+
+
+##### golang
+export GOROOT=/usr/local/go
+export GOPATH=/Users/guna/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+function go-build-linux {
+	if [ "$1" = "" ]; then
+	env GOOS=linux GOARCH=amd64 go build -o main-linux
+	else
+	env GOOS=linux GOARCH=amd64 go build -o $1-linux
+	fi
+}
+
+##### redis
+alias redis-start="launchctl start io.redis.redis-server"
+alias redis-stop="launchctl stop io.redis.redis-server"
+
+##### mysql
+function start-mysql {
+	UP=$(pgrep mysql | wc -l);
+	if [ "$UP" -ne 1 ];
+	then
+	        echo "MySQL is down.";
+	        mysql.server start
+
+	else
+	        echo "MySQL is already up.";
+	fi
+}
+
+##### Docker
+function docker-stop-all {
+	docker stop $(docker ps -aq)
+}
+
+function docker-start-all {
+	docker start $(docker ps -aq)
+}
+
+function docker-rm-all-containers {
+	docker rm $(docker ps -aq)
+}
+
+function docker-rm-all-images {
+	docker rmi $(docker images -aq)
+}
+
+function docker-ips {
+	docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
+}
+
+##### youtube-dl
+
+function dl-audio {
+	youtube-dl -x --audio-format mp3 --audio-quality 0 "$1"
+}
+
+function dl-video {
+	youtube-dl "$1"
+}
+
+##### custom
+function play_sound {
+	if hash aplay 2>/dev/null; then
+	    aplay "$@"
+	else
+	    afplay "$@"
+	fi
+}
+
+function get-audio {
+	local pacdies=~/.pacdies.mp3
+	if ! [ -f $pacdies ]; then
+		wget "https://raw.githubusercontent.com/gunasekar/custom/master/shell/pacdies.mp3" -O $pacdies
+	fi
+
+	echo $pacdies
+}
+
+function notify-on-completion-fg {
+	pacdies=$(get-audio)
+	if [[ $# -gt 0 ]]; then
+		# Assume a PID has been passed
+		while [[ $(ps -e | grep $1 | wc -l) != "0" ]]; do
+		sleep 1
+		done
+
+		play_sound $pacdies
+	else
+		play_sound $pacdies
+	fi
+}
+
+function notify-on-completion {
+	notify-on-completion-fg "$@" &
+}
+
+function notify-after-fg {
+	pacdies=$(get-audio)
+	if [[ $# -gt 0 ]]; then
+		# Assume number of seconds has been passed
+		sleep $1
+
+		play_sound $pacdies
+	else
+		play_sound $pacdies
+	fi
+}
+
+function notify-after {
+	notify-after-fg "$@" &
+}
